c515d0c440b71d0a90dda267bbfd1f1e03f583b9
Guillermo Sanchez Sanz <guillermomonterde@gmail.com>
2022-11-23 17:49:43 +0100

    excepciones personalizadas

---
+++ PedroPinturas/Exceptions/PhoneException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PacoPinturas.Exceptions
+{
+    internal class PhoneException : Exception
+    {
+        //Excepción de teléfono
+        public PhoneException(string message) : base(message) { }
+    }
+}
---
+++ PedroPinturas/Exceptions/UsernameAlreadyExistException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PacoPinturas.Exceptions
+{
+    internal class UsernameAlreadyExistException : Exception
+    {
+        //Nombre de usuario ya existe
+        public UsernameAlreadyExistException(string message) : base(message) { }
+    }
+}
