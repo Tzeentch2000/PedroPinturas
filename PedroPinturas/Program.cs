a2988d732db68baf695db0bf30ba2509393ba25e
diego <djtasio2002@gmail.com>
2022-11-23 17:17:55 +0100

    proyecto inicial

---
+++ PedroPinturas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PedroPinturas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
