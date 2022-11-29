6d8a60be1c0be17200559cb0a1fa6b53dcbc443b
diego <djtasio2002@gmail.com>
2022-11-29 20:13:41 +0100

    creacion del program donde se encuentra el main

--- PedroPinturas/Program.cs
+++ PedroPinturas/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using PedroPinturas.Exceptions;
+using PedroPinturas.Functions;
+using PedroPinturas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Transactions;
 
 namespace PedroPinturas
 {
@@ -6,7 +13,39 @@ namespace PedroPinturas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Develop!");
+            List<Usuario> users = new List<Usuario>();
+            Usuario usuario = new Usuario();
+
+            do
+            {
+                int number = Metodos.CheckNumber(DisplayMenu.Initial(), 2);
+                switch (number)
+                {
+                    case 1:
+                        try
+                        {
+                            //Login
+                            usuario = DisplayInteractiveMenu.login(users);
+                            Console.WriteLine($"Bienvenido {usuario.NombreApellidos}");
+                            //metodo
+                            DisplayInteractiveMenu.InitialMenu(usuario);
+                        }
+                        catch (IncorrectUserException e)
+                        {
+                            Console.WriteLine(e.Message);
+                            Metodos.Olog.Add(e.Message);
+                        }
+                        break;
+                    case 2:
+                        //Register
+                        usuario = DisplayInteractiveMenu.Registrarse(users);
+                        Console.WriteLine("¡Usuario registrado con éxito!");
+                        users.Add(usuario);
+                        DisplayInteractiveMenu.InitialMenu(usuario);
+                        break;
+                        //metodo
+                }
+            } while (true);
         }
     }
 }
