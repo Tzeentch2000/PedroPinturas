dfbcbcf3d0e1c48b2f202c58349fd41875c14d22
diego <djtasio2002@gmail.com>
2022-11-29 19:34:45 +0100

    dos clases mas de excepciones

---
+++ PedroPinturas/Exceptions/EmptyCharacterExceptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PedroPinturas.Exceptions
+{
+    internal class EmptyCharacterExceptions : Exception
+    {
+        public EmptyCharacterExceptions(string message) : base(message) { }
+    }
+}
---
+++ PedroPinturas/Exceptions/OutOfRangeException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PedroPinturas.Exceptions
+{
+    internal class OutOfRangeException : Exception
+    {
+        public OutOfRangeException(string message) : base(message) { }
+    }
+}
